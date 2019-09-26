de2a50d88705e710622c22debf508e24c973f92f
Guy Levi <guy_levi@cs.huji.ac.il>
2019-09-26 19:49:30 +0300

    Tested image processor preprocessing stage

--- server/image_processor.py
+++ server/image_processor.py
@@ -12,71 +12,88 @@ matcher = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
 
 
 def register(images, base_index):
-	reference_image_color = images(base_index)
-	reference_image = cv2.cvtColor(reference_image_color, cv2.COLOR_BGR2GRAY)
-	height, width = reference_image.shape
-	for i in range(len(images)):
-		if i == base_index:
-			continue
-		image_color = images(i)
-		image = cv2.cvtColor(image_color, cv2.COLOR_BGR2GRAY)
-		image_kp, image_d = orb_detector.detectAndCompute(image, None)
-		ref_kp, ref_d = orb_detector.detectAndCompute(reference_image, None)
-		matches = matcher.match(image_d, ref_d)
-		matches.sort(key=lambda x: x.distance)
-		matches = matches[:int(len(matches) * 90)]
-		no_of_matches = len(matches)
-		p1 = np.zeros((no_of_matches, 2))
-		p2 = np.zeros((no_of_matches, 2))
-		for j in range(len(matches)):
-			p1[j, :] = image_kp[matches[j].queryIdx].pt
-			p2[j, :] = ref_kp[matches[j].trainIdx].pt
-		homography, mask = cv2.findHomography(p1, p2, cv2.RANSAC)
-		transformed_img = cv2.warpPerspective(image_color, homography, (width, height))
-		images[i] = transformed_img
+    """
+	Homographically aligns images to the perspective of the base image
+
+	:param images: Images to align
+	:param base_index: Index of the base image
+	"""
+    reference_image_color = images[base_index]
+    reference_image = cv2.cvtColor(reference_image_color, cv2.COLOR_BGR2GRAY)
+    height, width = reference_image.shape
+    for i in range(len(images)):
+        if i == base_index:
+            continue
+        image_color = images[i]
+        image = cv2.cvtColor(image_color, cv2.COLOR_BGR2GRAY)
+        image_kp, image_d = orb_detector.detectAndCompute(image, None)
+        ref_kp, ref_d = orb_detector.detectAndCompute(reference_image, None)
+        matches = matcher.match(image_d, ref_d)
+        matches.sort(key=lambda x: x.distance)
+        matches = matches[:int(len(matches) * 90)]
+        no_of_matches = len(matches)
+        p1 = np.zeros((no_of_matches, 2))
+        p2 = np.zeros((no_of_matches, 2))
+        for j in range(len(matches)):
+            p1[j, :] = image_kp[matches[j].queryIdx].pt
+            p2[j, :] = ref_kp[matches[j].trainIdx].pt
+        homography, mask = cv2.findHomography(p1, p2, cv2.RANSAC)
+        transformed_img = cv2.warpPerspective(image_color, homography, (width, height))
+        images[i] = transformed_img
 
 
 def get_faces(image):
-	f, b = [], []
-	grey = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-	faces = cascade.detectMultiScale(
-		grey,
-		scaleFactor=1.1,
-		minNeighbors=5,
-		minSize=(30, 30),
-		flags=cv2.CASCADE_SCALE_IMAGE
-	)
-	for (x, y, w, h) in faces:
-		f.append(image[y:y + h, x:x + w])
-		b.append((x, y, w, h))
-	return f, b
+    f, b = [], []
+    grey = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    faces = cascade.detectMultiScale(
+        grey,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30, 30),
+        flags=cv2.CASCADE_SCALE_IMAGE
+    )
+    for (x, y, w, h) in faces:
+        f.append(image[y:y + h, x:x + w])
+        b.append((x, y, w, h))
+    return f, b
 
 
 class Imutils:
-	images = []
-	boxes = []
-	faces = []
+    ready = False
+    output = None
+    images = []
+    boxes = []
+    faces = []
 
-	def add_image(self, image):
-		self.images.append(image)
-		if len(self.images) > MAX_IMAGES:
-			raise Exception('Exceeded max images')
-	
-	def clear_images(self):
-		self.images = []
+    def add_image(self, image):
+        if len(self.images) >= MAX_IMAGES:
+            raise Exception('Exceeded max images')
+        self.images.append(image)
+        self.ready = False
 
-	def preprocess(self, base_index):
-		"""
+    def reset(self):
+        self.images = []
+        self.boxes = []
+        self.faces = []
+        self.output = None
+        self.ready = False
+
+    def preprocess(self, base_index):
+        """
 		Aligns images
 		Populates boxes with a list of bounding boxes for each image
 		Populates faces with a list of cropped faces for each image
 		:param base_index:
 		:return:
 		"""
-		if len(self.images) < MIN_IMAGES: 
-			raise Exception('Not enough images to preprocess')
-		register(self.images, base_index)
-		for image in self.images:
-			f, b = get_faces(image)
-			self.faces.append(f)
-			self.boxes.append(b)
+        if len(self.images) < MIN_IMAGES:
+            raise Exception('Not enough images to preprocess')
+        register(self.images, base_index)
+        for image in self.images:
+            f, b = get_faces(image)
+            self.faces.append(f)
+            self.boxes.append(b)
+        self.ready = True
+
+    def process(self, indexes):
+        pass
---
+++ server/test.py
@@ -0,0 +1,18 @@
+import image_processor as ip
+import cv2
+
+im1 = cv2.imread("0.jpg")
+im2 = cv2.imread("1.jpg")
+im3 = cv2.imread("2.jpg")
+
+imutils = ip.Imutils()
+
+imutils.add_image(im1)
+imutils.add_image(im2)
+imutils.add_image(im3)
+
+imutils.preprocess(1)
+
+cv2.imwrite("o0.jpg", imutils.images[0])
+cv2.imwrite("o1.jpg", imutils.images[1])
+cv2.imwrite("o2.jpg", imutils.images[2])
